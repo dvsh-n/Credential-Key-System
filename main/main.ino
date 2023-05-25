0d288905db95320b642e4cbe97d9d090be3aab09
Devesh Nath <deveshnath101@gmail.com>
2023-05-25 14:09:30 -0700

    Added fingerprint setup code
    refine the code later

--- main/main.ino
+++ main/main.ino
@@ -14,6 +14,8 @@
 3 -> Reset current process
 */
 
+#include <Adafruit_Fingerprint.h>
+#include <HardwareSerial.h>
 
 // Variables and definitions
 uint8_t nextState = 0; 
@@ -24,6 +26,9 @@ uint8_t verified = 0;
 String input;
 const String password = "espTest";
 const String codes[numCodes] = {"login", "logout", "chgPass", "enroll", "delete"}; // Task indexes are important, they are important for switch.
+
+
+Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial2);
 
 // Functions
 uint8_t getTaskIdx() {
@@ -54,6 +59,38 @@ void waitAndGetInput() {
   }
 }
 
+void setupFingerprintSensor() {
+  // set the data rate for the sensor serial port
+  finger.begin(57600);
+  delay(5);
+  if (finger.verifyPassword()) {
+    Serial.println("Found fingerprint sensor!");
+  } else {
+    Serial.println("Did not find fingerprint sensor :(");
+    while (1) { delay(1); }
+  }
+
+  Serial.println(F("Reading sensor parameters"));
+  finger.getParameters();
+  Serial.print(F("Status: 0x")); Serial.println(finger.status_reg, HEX);
+  Serial.print(F("Sys ID: 0x")); Serial.println(finger.system_id, HEX);
+  Serial.print(F("Capacity: ")); Serial.println(finger.capacity);
+  Serial.print(F("Security level: ")); Serial.println(finger.security_level);
+  Serial.print(F("Device address: ")); Serial.println(finger.device_addr, HEX);
+  Serial.print(F("Packet len: ")); Serial.println(finger.packet_len);
+  Serial.print(F("Baud rate: ")); Serial.println(finger.baud_rate);
+
+  finger.getTemplateCount();
+
+  if (finger.templateCount == 0) {
+    Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
+  }
+  else {
+    Serial.println("Waiting for valid finger...");
+      Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+  }
+}
+
 // Setup
 void setup() {
   Serial.begin(115200);
@@ -61,6 +98,7 @@ void setup() {
   while (!Serial) {
     ;  
   }
+  setupFingerprintSensor();
 }
 
 // Loop
